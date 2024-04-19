823f65faa0a531d20fff33b7f4d4ec44f154c1bd
anvv <manhvdm@gmal.com>
2024-04-19 23:54:26 +0700

    1

---
+++ Ôn tập/30.py
@@ -0,0 +1,6 @@
+# Phát triển một chương trình để mô phỏng hệ thống chống xâm nhập sử dụng cảm biến siêu âm.
+# Hệ thống sẽ theo dõi và phát hiện sự hiện diện của vật cản phía trước.
+# Khi phát hiện vật cản, hệ thống bật đèn LED như một cảnh báo ban đầu.
+# Khi khoảng cách giữa cảm biến và vật cản nhỏ hơn một ngưỡng xác định, hệ thống sẽ kích hoạt relay 1.
+# Nếu tiếp tục sẽ làm quay động cơ DC để tăng cấp độ cảnh báo.
+# Hiển thị các mức độ cảnh báo và thông tin về khoảng cách đến vật cản trên màn hình LCD.
