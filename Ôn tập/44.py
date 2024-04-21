4b3c36c1b125980f73e09200f429ebd43bb69b21
anvv <manhvdm@gmal.com>
2024-04-21 10:49:31 +0700

    2

---
+++ Ôn tập/44.py
@@ -0,0 +1,10 @@
+# Phát triển một mô hình thang máy mô phỏng điều khiển cho một tòa nhà 4 tầng, sử dụng bo mạch thực hành.
+# Mô hình sẽ tích hợp các nút bấm để gọi thang, một động cơ DC để mô phỏng việc thang máy di chuyển lên/xuống,
+# một động cơ RC servo để mô phỏng cửa thang mở/đóng và sử dụng LED matrix cùng màn hình LCD để hiển thị
+# thông tin về vị trí hiện tại và trạng thái của thang máy. Mô tả chi tiết: Tích hợp 4 nút bấm tương ứng với mỗi tầng
+# trong tòa nhà. Khi một nút được nhấn, hệ thống sẽ xử lý yêu cầu và di chuyển thang máy đến tầng đó.
+# Sử dụng động cơ DC để mô phỏng việc thang máy di chuyển lên hoặc xuống. Chiều quay của động cơ sẽ thay đổi tùy theo
+# hướng di chuyển của thang máy. Sử dụng động cơ RC servo để mô phỏng việc mở và đóng cửa thang máy.
+# Đảm bảo có thời gian chờ trước khi cửa đóng để mô phỏng việc chờ khách. Sử dụng LED matrix để hiển thị
+# số tầng hiện tại của thang máy. Sử dụng màn hình LCD để hiển thị trạng thái hoạt động của thang máy,
+# bao gồm các thông báo như: "đang chờ", "đang di chuyển lên", "đang di chuyển xuống", và "cửa mở".
