455136c1ddc83c675e46d2eb061f36c368afd029
anvv <manhvdm@gmal.com>
2024-04-22 13:01:07 +0700

    3

---
+++ Ôn tập/45.py
@@ -0,0 +1,6 @@
+# Phát triển một mô hình điều khiển tự động cho nhiệt độ và độ ẩm trong kho bảo quản đông lạnh, sử dụng cảm biến
+# DHT11 để theo dõi môi trường, các button để cài đặt thông số mong muốn, và các relay để điều khiển thiết bị
+# công suất giúp giữ môi trường ổn định. Mô tả chi tiết: Sử dụng cảm biến DHT11 để đo nhiệt độ và độ ẩm trong kho.
+# Dữ liệu thu thập sẽ được dùng để điều chỉnh môi trường bảo quản. Button được sử dụng để cài đặt các giới hạn nhiệt độ
+# và độ ẩm mong muốn. Các thông số nhiệt độ, độ ẩm được hiển thị trên màn hình LCD. Để ổn định nhiệt độ, độ ẩm,
+# chúng ta sử dụng các thiết bị công suất thông qua các relay
